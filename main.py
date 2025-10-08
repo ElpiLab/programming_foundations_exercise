8e5b6af2536b38780c880e03c25d3f7ed4b82dcb
Elpidio <elpidio.dogbevi@gmail.com>
2025-10-08 09:35:37 +0000

    Class08.10

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
 
+
+#for name in ["Philip", "Anna", "Peter", "Eliza"]:
+    #print(name)
+
+#print(f"The last name is: {name}")
+
+
+#Coundtdwon
+
+#import time
+#for sec in range (10, -1, -1):
+    #print(sec)
+    #time.sleep(1)
+
+
+## Order in Restaurant
+
+#Foodlist = ["Pasta", "Reis"]
+prices = [11.20, 35.40, 14.20]
+for number in prices:
+    #print(prices(sum)
+    print(sum(prices))
+
+# While for a condition
+# For Loop: we know how often
+
+name = input("What is your name? ")
+
+while not name:
+    print("You have not given me a name")
+    name = input("What is your name?")
+
+        #Nested Loop Loop inside Loop
+
+numbers = [1,2,3,4,5]
+
+for x in numbers:
+    for y in numbers
+        print(f"{x}*{y} = {x*y}")
+    print("_"*20)
+
+## Break
+# Break the loop
